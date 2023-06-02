f32fa46a84a520fa78221a9a6ec293e275eec2e6
petrisoralexandru <kwixacademy@gmail.com>
2023-06-02 21:10:21 +0300

    Added SaveData method for the data access layer

--- DataAccessLibrary/SqlDataAccess.cs
+++ DataAccessLibrary/SqlDataAccess.cs
@@ -7,7 +7,7 @@ namespace DataAccessLibrary;
 public class SqlDataAccess
 {
     /// <summary>
-    ///     The method is responsible for querying the data
+    ///     The method is responsible for querying the data. Ideal for reading data.
     /// </summary>
     /// <param name="sql">SQL query written as a string</param>
     /// <param name="parameters">Parameters for performing a parameterized query</param>
@@ -21,6 +21,21 @@ public class SqlDataAccess
         {
             List<T> rows = connection.Query<T>(sql, parameters).ToList();
             return rows;
+        }
+    }
+    
+    /// <summary>
+    ///     The method is responsible for executing queries to the database. Ideal for creating, updating, deleting data.
+    /// </summary>
+    /// <param name="sql">SQL query written as a string</param>
+    /// <param name="parameters">Parameters for performing a parameterized query</param>
+    /// <param name="connectionString">String indicating database connection details</param>
+    /// <typeparam name="T">Parameter(s) specified to transmit parameterized into query</typeparam>
+    public void SaveData<T>(string sql, T parameters, string connectionString)
+    {
+        using (IDbConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Execute(sql, parameters);
         }
     }
 }
