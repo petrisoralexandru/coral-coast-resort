8559e97d0671842aefc82d30f91382acb25aa6a4
petrisoralexandru <kwixacademy@gmail.com>
2023-06-03 22:51:19 +0300

    Added RoomTypes class model

---
+++ DataAccessLibrary/Models/RoomTypesModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DataAccessLibrary.Models;
+
+/// <summary>
+///     Model class that holds all the properties of the RoomTypes table.
+/// </summary>
+public class RoomTypesModel
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+}
