5ae65685ce81ec760a67adcaec04e6f54d14df23
petrisoralexandru <kwixacademy@gmail.com>
2023-06-03 20:08:01 +0300

    Modified the SaveData method to take the connection string though dependency injection and identify if the method use stored procedures

--- DataAccessLibrary/SqlDataAccess.cs
+++ DataAccessLibrary/SqlDataAccess.cs
@@ -40,19 +40,28 @@ public class SqlDataAccess
             return rows;
         }
     }
-    
+
     /// <summary>
     ///     The method is responsible for executing queries to the database. Ideal for creating, updating, deleting data.
     /// </summary>
     /// <param name="sql">SQL query written as a string</param>
     /// <param name="parameters">Parameters for performing a parameterized query</param>
-    /// <param name="connectionString">String indicating database connection details</param>
+    /// <param name="connectionStringName">String indicating database connection details</param>
+    /// <param name="options"></param>
     /// <typeparam name="T">Parameter(s) specified to transmit parameterized into query</typeparam>
-    public void SaveData<T>(string sql, T parameters, string connectionString)
+    public void SaveData<T>(string sql, T parameters, string connectionStringName, dynamic? options = null)
     {
+        string? connectionString = _config.GetConnectionString(connectionStringName);
+        CommandType commandType = CommandType.Text;
+        
+        if (options?.IsStoredProcedure != null && options?.IsStoredProcedure == true)
+        {
+            commandType = CommandType.StoredProcedure;
+        }
+        
         using (IDbConnection connection = new SqlConnection(connectionString))
         {
-            connection.Execute(sql, parameters);
+            connection.Execute(sql, parameters, commandType: commandType);
         }
     }
 }
