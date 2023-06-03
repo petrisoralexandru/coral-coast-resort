1919b4a275c0a5c62c5cb0841d52fef52fe78178
petrisoralexandru <kwixacademy@gmail.com>
2023-06-03 19:42:41 +0300

    Modified the LoadData method to take the connection string though dependency injection

--- DataAccessLibrary/SqlDataAccess.cs
+++ DataAccessLibrary/SqlDataAccess.cs
@@ -1,22 +1,32 @@
 ﻿using System.Data;
 using System.Data.SqlClient;
 using Dapper;
+using Microsoft.Extensions.Configuration;
 
 namespace DataAccessLibrary;
 
 public class SqlDataAccess
 {
+    private readonly IConfiguration _config;
+    
+    public SqlDataAccess(IConfiguration config)
+    {
+        _config = config;
+    }
+    
     /// <summary>
     ///     The method is responsible for querying the data. Ideal for reading data.
     /// </summary>
     /// <param name="sql">SQL query written as a string</param>
     /// <param name="parameters">Parameters for performing a parameterized query</param>
-    /// <param name="connectionString">String indicating database connection details</param>
+    /// <param name="connectionStringName">String indicating database connection details</param>
     /// <typeparam name="T">The class model to be queried</typeparam>
     /// <typeparam name="U">Parameter(s) specified to transmit parameterized into query</typeparam>
     /// <returns>One or more database rows transformed as objects</returns>
-    public List<T> LoadData<T, U>(string sql, U parameters, string connectionString)
+    public List<T> LoadData<T, U>(string sql, U parameters, string connectionStringName)
     {
+        string? connectionString = _config.GetConnectionString(connectionStringName);
+        
         using (IDbConnection connection = new SqlConnection(connectionString))
         {
             List<T> rows = connection.Query<T>(sql, parameters).ToList();
