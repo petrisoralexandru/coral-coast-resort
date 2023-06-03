91a16bc9fdece4477bd075a809cabfae22a8c43b
petrisoralexandru <kwixacademy@gmail.com>
2023-06-03 19:56:57 +0300

    Added code logic to identify if the LoadData is using a stored procedure or a standard sql statement

--- DataAccessLibrary/SqlDataAccess.cs
+++ DataAccessLibrary/SqlDataAccess.cs
@@ -13,23 +13,30 @@ public class SqlDataAccess
     {
         _config = config;
     }
-    
+
     /// <summary>
     ///     The method is responsible for querying the data. Ideal for reading data.
     /// </summary>
     /// <param name="sql">SQL query written as a string</param>
     /// <param name="parameters">Parameters for performing a parameterized query</param>
     /// <param name="connectionStringName">String indicating database connection details</param>
+    /// <param name="options">Parameter specifying whether to execute a query as a stored procedure</param>
     /// <typeparam name="T">The class model to be queried</typeparam>
     /// <typeparam name="U">Parameter(s) specified to transmit parameterized into query</typeparam>
     /// <returns>One or more database rows transformed as objects</returns>
-    public List<T> LoadData<T, U>(string sql, U parameters, string connectionStringName)
+    public List<T> LoadData<T, U>(string sql, U parameters, string connectionStringName, dynamic? options = null)
     {
         string? connectionString = _config.GetConnectionString(connectionStringName);
+        CommandType commandType = CommandType.Text;
+
+        if (options?.IsStoredProcedure != null && options?.IsStoredProcedure == true)
+        {
+            commandType = CommandType.StoredProcedure;
+        }
         
         using (IDbConnection connection = new SqlConnection(connectionString))
         {
-            List<T> rows = connection.Query<T>(sql, parameters).ToList();
+            List<T> rows = connection.Query<T>(sql, parameters, commandType: commandType).ToList();
             return rows;
         }
     }
