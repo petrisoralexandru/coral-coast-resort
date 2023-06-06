616efb1cbf85553dc9f1e488f19a4292c704c03d
petrisoralexandru <kwixacademy@gmail.com>
2023-06-06 17:31:43 +0300

    Added the guest model to hold all the information about a hotel guest

---
+++ DataAccessLibrary/Models/GuestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DataAccessLibrary.Models;
+
+/// <summary>
+///     Model class that holds all the properties of the hotel guest
+/// </summary>
+public class GuestModel
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
