af08f8a70bf61f911c921957aa9d6f3fb0bcb87d
petrisoralexandru <kwixacademy@gmail.com>
2023-06-06 15:17:02 +0300

    Added the GetAvailableRoomTypes method to view the hotel room types available to book in a certain calendaristic period

---
+++ DataAccessLibrary/SqlData.cs
@@ -0,0 +1,26 @@
+﻿using DataAccessLibrary.Models;
+
+namespace DataAccessLibrary;
+
+public class SqlData
+{
+    private readonly ISqlDataAccess _db;
+    private const string ConnectionStringName = "SqlServerDB";
+
+    public SqlData(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+    
+    /// <summary>
+    ///     The function is responsible for displaying available room types in a calendar range
+    /// </summary>
+    /// <param name="startDate">Check-in day</param>
+    /// <param name="endDate">Check-out day</param>
+    /// <returns>Returns a list of room types available in a given calendar date range</returns>
+    public List<RoomTypesModel> GetAvailableRoomTypes(DateOnly startDate, DateOnly endDate)
+    {
+        return _db.LoadData<RoomTypesModel, dynamic>("dbo.RoomTypes_GetAvailableTypes", new { startDate, endDate },
+            ConnectionStringName, true);
+    }
+}
