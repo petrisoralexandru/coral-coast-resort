10f0a00393ad31090fb12302045ed88c2339566c
petrisoralexandru <kwixacademy@gmail.com>
2023-06-06 20:52:42 +0300

    Added a booking method to be able to book a certain hotel room based on various criteria

--- DataAccessLibrary/SqlData.cs
+++ DataAccessLibrary/SqlData.cs
@@ -11,7 +11,7 @@ public class SqlData
     {
         _db = db;
     }
-    
+
     /// <summary>
     ///     The function is responsible for displaying available room types in a calendar range
     /// </summary>
@@ -21,6 +21,42 @@ public class SqlData
     public List<RoomTypesModel> GetAvailableRoomTypes(DateOnly startDate, DateOnly endDate)
     {
         return _db.LoadData<RoomTypesModel, dynamic>("dbo.RoomTypes_GetAvailableTypes", new { startDate, endDate },
+            ConnectionStringName, true);
+    }
+
+    /// <summary>
+    ///     The method that is responsible for booking a guest in a hotel room, based on the criteria of name, last name,
+    ///     date range and type of room desired, following the rooms availability.
+    /// </summary>
+    /// <param name="firstName">Guest's first name</param>
+    /// <param name="lastName">Guest's last name</param>
+    /// <param name="startDate">Check-in day</param>
+    /// <param name="endDate">Check-out day</param>
+    /// <param name="roomTypeId">Camera type identification Id</param>
+    public void BookGuest(string firstName, string lastName, DateTime startDate, DateTime endDate, int roomTypeId)
+    {
+        var guest = _db
+            .LoadData<GuestModel, dynamic>("dbo.Guests_Insert", new { firstName, lastName }, ConnectionStringName, true)
+            .First();
+
+        var roomType =
+            _db.LoadData<RoomTypesModel, dynamic>("dbo.RoomTypes_GetRoomTypeById", new { roomTypeId },
+                ConnectionStringName, true).First();
+
+        var timeStaying = endDate.Date.Subtract(startDate.Date);
+
+        var availableRooms = _db.LoadData<RoomModel, dynamic>("dbo.Rooms_GetAvailableRooms",
+            new { startDate, endDate, roomTypeId }, ConnectionStringName, true);
+
+        _db.SaveData("dbo.Bookings_Insert",
+            new
+            {
+                roomId = availableRooms.First().Id,
+                guestId = guest.Id,
+                startDate,
+                endDate,
+                totalCost = timeStaying.Days * roomType.Price
+            },
             ConnectionStringName, true);
     }
 }
