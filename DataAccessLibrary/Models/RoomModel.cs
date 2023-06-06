6fe7c41c637e63761efa4709fcc13eb3552d825f
petrisoralexandru <kwixacademy@gmail.com>
2023-06-06 18:23:11 +0300

    Added a room model class that holds the information about a hotel room

---
+++ DataAccessLibrary/Models/RoomModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DataAccessLibrary.Models;
+
+/// <summary>
+///     Model class that holds all the properties of a hotel room
+/// </summary>
+public class RoomModel
+{
+    public int Id { get; set; }
+    public int RoomNumber { get; set; }
+    public int RoomTypeId { get; set; }
+}
