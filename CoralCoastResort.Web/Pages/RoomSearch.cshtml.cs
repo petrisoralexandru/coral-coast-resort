a9435579a0cf01d019ab36dd590462b09693415f
petrisoralexandru <kwixacademy@gmail.com>
2023-06-08 22:21:09 +0300

    Added the search for a hotel room page design and logic code

---
+++ CoralCoastResort.Web/Pages/RoomSearch.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoralCoastResort.Web.Pages;
+
+public class RoomSearch : PageModel
+{
+    [DataType(DataType.Date)]
+    [BindProperty]
+    public DateTime StartDate { get; set; } = DateTime.Now;
+
+    [DataType(DataType.Date)]
+    [BindProperty]
+    public DateTime EndDate { get; set; } = DateTime.Now.AddDays(1);
+    
+    public void OnGet()
+    {
+        
+    }
+
+    public IActionResult OnPost()
+    {
+        return Page();
+    }
+}
