c5839a45da04c14f6079c086637072c317cbc35d
petrisoralexandru <kwixacademy@gmail.com>
2023-06-09 15:06:44 +0300

    Added the C# logic code to be able to fetch the available room tpyes in a specific date interval

--- CoralCoastResort.Web/Pages/RoomSearch.cshtml.cs
+++ CoralCoastResort.Web/Pages/RoomSearch.cshtml.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using DataAccessLibrary.Data;
+using DataAccessLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,21 +8,36 @@ namespace CoralCoastResort.Web.Pages;
 
 public class RoomSearch : PageModel
 {
+    private readonly IDatabaseData _db;
+
+    public RoomSearch(IDatabaseData db)
+    {
+        _db = db;
+    }
+
     [DataType(DataType.Date)]
-    [BindProperty]
+    [BindProperty(SupportsGet = true)]
     public DateTime StartDate { get; set; } = DateTime.Now;
 
     [DataType(DataType.Date)]
-    [BindProperty]
+    [BindProperty(SupportsGet = true)]
     public DateTime EndDate { get; set; } = DateTime.Now.AddDays(1);
-    
+
+    [BindProperty(SupportsGet = true)] 
+    public bool SearchComplete { get; set; } = false;
+
+    public List<RoomTypesDto> AvailableRoomTypes { get; set; }
+
     public void OnGet()
     {
-        
+        if (SearchComplete)
+        {
+            AvailableRoomTypes = _db.GetAvailableRoomTypes(StartDate, EndDate);
+        }
     }
 
     public IActionResult OnPost()
     {
-        return Page();
+        return RedirectToPage(new { SearchComplete = true, StartDate, EndDate });
     }
 }
