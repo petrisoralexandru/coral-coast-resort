a317b78f7dff08710ed4b09f87eca60371e7b651
petrisoralexandru <kwixacademy@gmail.com>
2023-06-09 19:57:55 +0300

    Added the necessary class members to be able to book a guest in a specified room type

--- CoralCoastResort.Web/Pages/BookRoom.cshtml.cs
+++ CoralCoastResort.Web/Pages/BookRoom.cshtml.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLibrary.Data;
+using DataAccessLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CoralCoastResort.Web.Pages;
 
 public class BookRoom : PageModel
 {
+    private readonly IDatabaseData _db;
+
+    public BookRoom(IDatabaseData db)
+    {
+        _db = db;
+    }
+
     [BindProperty]
     public string? FirstName { get; set; }
 
@@ -19,14 +28,20 @@ public class BookRoom : PageModel
 
     [BindProperty(SupportsGet = true)]
     public DateTime EndDate { get; set; }
+
+    public RoomTypesDto? RoomType { get; set; }
     
     public void OnGet()
     {
-        
+        if (RoomTypeId > 0)
+        {
+            RoomType = _db.GetRoomTypeById(RoomTypeId);
+        }
     }
 
     public IActionResult OnPost()
     {
+        _db.BookGuest(FirstName, LastName, StartDate, EndDate, RoomTypeId);
         return RedirectToPage("./Index");
     }
 }
