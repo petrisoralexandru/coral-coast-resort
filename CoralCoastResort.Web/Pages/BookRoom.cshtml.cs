2108fefddff76b352aba68372bc7620ec4f96447
petrisoralexandru <kwixacademy@gmail.com>
2023-06-09 15:34:18 +0300

    Added the booking functionality page

---
+++ CoralCoastResort.Web/Pages/BookRoom.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoralCoastResort.Web.Pages;
+
+public class BookRoom : PageModel
+{
+    [BindProperty]
+    public string? FirstName { get; set; }
+
+    [BindProperty]
+    public string? LastName { get; set; }
+
+    [BindProperty(SupportsGet = true)]
+    public int RoomTypeId { get; set; }
+
+    [BindProperty(SupportsGet = true)]
+    public DateTime StartDate { get; set; }
+
+    [BindProperty(SupportsGet = true)]
+    public DateTime EndDate { get; set; }
+    
+    public void OnGet()
+    {
+        
+    }
+}
