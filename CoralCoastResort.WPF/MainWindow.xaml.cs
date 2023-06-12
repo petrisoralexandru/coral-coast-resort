ef985006f973acb924a2ee75e6f28d75f406a445
petrisoralexandru <kwixacademy@gmail.com>
2023-06-12 21:04:12 +0300

    Added a list of bookings

--- CoralCoastResort.WPF/MainWindow.xaml.cs
+++ CoralCoastResort.WPF/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 using DataAccessLibrary.Data;
+using DataAccessLibrary.Models;
 
 namespace CoralCoastResort.WPF;
 
@@ -14,5 +16,11 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         _db = db;
+    }
+
+    private void SearchForGuest_OnClick(object sender, RoutedEventArgs e)
+    {
+        List<BookingsDto> bookings = _db.SearchBookings(FirstNameText.Text);
+        ResultsList.ItemsSource = bookings;
     }
 }
